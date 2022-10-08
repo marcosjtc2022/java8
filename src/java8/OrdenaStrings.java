f94d96455a13978ec69ecb085f7e414897d16026
Marcos Cardoso <marcosjtc1973@gmail.com>
2022-10-08 12:43:12 -0300

    Interfaces funcionais lambda

--- src/java8/OrdenaStrings.java
+++ src/java8/OrdenaStrings.java
@@ -9,68 +9,145 @@ import java.util.function.Consumer;
 //CTRL + SHIFT + F organiza o código.
 public class OrdenaStrings {
 	public static void main(String[] args) {
-		
-		//Classe String utiliza comaparable ordena de [A-Z].
+
+		// Classe String utiliza comaparable ordena de [A-Z].
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
-		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		//Comparação feita pela classe string. 
-		//Collections.sort(palavras);
-		//Nova Comparação feita com o método comparador 
-		//Collections.sort(palavras,comparador);
-		//Nova comparação o método sort da classe List.
-		//Agora as interfaces tem métodos concretos. (Java8).
-		//São chamados default methods que permitem a evolução das 
-		//interfaces sem que quebrem as classes antigas.
-		//É uma estratégia de evolução de interfaces.
-		//Métodos default permitem que sejam inseridos métodos 
-		//nas interfaces, e que estes métodos tenham corpo.
-		//É diferente de uma classe abastrata que pode ter atributos,
-		//pode ter estados. Uma interface não pode ter estado, mas,
-		//apenas métodos concretos.
-		palavras.sort(comparador);
-		
+
+		// passa direto em palvras.sort(comparador);
+		// Comparator<String> comparador = new ComparadorPorTamanho();
+
+		// Comparação feita pela classe string.
+		// Collections.sort(palavras);
+		// Nova Comparação feita com o método comparador
+		// Collections.sort(palavras,comparador);
+		// Nova comparação o método sort da classe List.
+		// Agora as interfaces tem métodos concretos. (Java8).
+		// São chamados default methods que permitem a evolução das
+		// interfaces sem que quebrem as classes antigas.
+		// É uma estratégia de evolução de interfaces.
+		// Métodos default permitem que sejam inseridos métodos
+		// nas interfaces, e que estes métodos tenham corpo.
+		// É diferente de uma classe abastrata que pode ter atributos,
+		// pode ter estados. Uma interface não pode ter estado, mas,
+		// apenas métodos concretos.
+		// palavras.sort(comparador);
+		// Transformar em um lambda que funciona sempre para uma interface que tem um
+		// método abstrato (Interface funcional).
+//		palavras.sort(new Comparator<String>() {
+//
+//			@Override
+//			public int compare(String s1, String s2) {
+//				if (s1.length() < s2.length())
+//					return -1;
+//				if(s1.length() > s2.length())
+//					return 1;
+//				return 0;
+//			}});
+		// ctrl + shift + f para formatar/endentar.
+//		palavras.sort((String s1, String s2) -> {
+//			if (s1.length() < s2.length())
+//				return -1;
+//			if (s1.length() > s2.length())
+//				return 1;
+//			return 0;
+//		});
+//		palavras.sort((s1,s2) -> {
+//			if (s1.length() < s2.length())
+//				return -1;
+//			if (s1.length() > s2.length())
+//				return 1;
+//			return 0;
+//		});
+
+//		palavras.sort((s1,s2) -> {
+//			return Integer.compare(s1.length(), s2.length());
+//		});
+        
+		//Princípio básico do lambda: consegue ser convertido para uma classe funcional.
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(palavras);
-		
-		for (String p: palavras) {
-			System.out.println(p);			
+
+		for (String p : palavras) {
+			System.out.println(p);
 		}
+
+		// For each recebe um consumidor de strings.
+		// forEach faz parte da classe Iterable.
+		// Todas as collections são da clase Iterable.
+		// Consumer<String> consumidor = new ImprimeNaLinha();
+		// Pode executar o new em um consumer de string diretamente
+		// Classe anônima retira a necessidade de delcarar a classe.
+		Consumer<String> consumidor = new Consumer<String>() {
+
+			@Override
+			public void accept(String s) {
+				System.out.println(" Nova implementação s " + s);
+
+			}
+		};
+
+		// palavras.forEach(consumidor);
+		// Pode passar toda a classe como parâmetro.
+//		palavras.forEach(new Consumer<String>() {
+//
+//			@Override
+//			public void accept(String s) {
+//				System.out.println(" Nova implementação s " + s);
+//				
+//			}});
+		// O compildor infere a classe anônima acima. (Lambda)
+//			palavras.forEach((String s) -> {
+//								System.out.println(s);
+//								
+//			});
+		// Só tem uma interface e um argumento.
+//			palavras.forEach(s -> {
+//				System.out.println(s);
+//				
+//});
+		palavras.forEach(s -> System.out.println(s));
 		
-		//For each recebe um consumidor de strings.
-		//forEach faz parte da classe Iterable.
-		//Todas as collections são da clase Iterable.
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		//Transformando em lambda. (Equivalente ao comando de cima).
+		Consumer<String> impressor  = s -> System.out.println(s);
+		palavras.forEach(impressor);
 		
+		//Não funciona por não ser uma interface funcional à esquerda.
+		//Object o = s -> System.out.println(s);
 		
+		//Não funciona pq o comparator exige dois argumentos e retorna um integer.
+		//E a expressão lambda recebe um argumento e devolve void.
+		//Expressão lambda Tem que receber argumento e devolve o mesmo tipo de parâmetro.
+		//Comparator<String> comparador = s -> System.out.println(s);
 
 	}
 
 }
 
-class ImprimeNaLinha implements Consumer<String>{
+//Classe anônima retira a necessidade de delcarar esta classe.
+class ImprimeNaLinha implements Consumer<String> {
 
 	@Override
 	public void accept(String s) {
 		System.out.println(" Nova implementação " + s);
-		
+
 	}
-	
+
 }
 
 //Obrigatoriamente tem que implementar a interface Comparator.
-class ComparadorPorTamanho implements Comparator<String>{
+class ComparadorPorTamanho implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
 		if (s1.length() < s2.length())
 			return -1;
-		if(s1.length() > s2.length())
+		if (s1.length() > s2.length())
 			return 1;
 		return 0;
 	}
-	
+
 }
